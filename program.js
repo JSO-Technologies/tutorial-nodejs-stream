b4850f0ddead7cba86c3fb1b7c47dac17a5554bc
JSO-Technologies <JSO.Technologies@gmail.com>
2015-01-22 14:59:33 +0100

    Pipe transform to uppercase

--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-process.stdin.pipe(process.stdout);
+var through = require('through');
+
+var uppercaseTransformer = function(buff) {
+    this.queue(buff.toString().toUpperCase());
+};
+
+process.stdin
+    .pipe(through(uppercaseTransformer))
+    .pipe(process.stdout);
