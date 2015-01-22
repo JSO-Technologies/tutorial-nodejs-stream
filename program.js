e63681fccfbfb65f5b98f0e93e4bce8684694e9b
JSO-Technologies <JSO.Technologies@gmail.com>
2015-01-22 16:55:13 +0100

    Pipe http client call to std in and out

--- program.js
+++ program.js
@@ -1,19 +1,6 @@
 var http = require('http');
-var through = require('through');
+var request = require('request');
 
-var upperCaseTranform = function(buff) {
-    this.queue(buff.toString().toUpperCase());
-};
-
-var server = http.createServer(function(req, res) {
-    if(req.method !== 'POST') {
-        res.writeHead(404, {'content-type': 'plain/text'});
-        res.write('Service not found : Only POST is supported');
-        res.end();
-    }
-    else {
-        req.pipe(through(upperCaseTranform)).pipe(res);
-    }
-});
-
-server.listen(process.argv[2]);
+process.stdin
+    .pipe(request.post('http://localhost:8000'))
+    .pipe(process.stdout);
