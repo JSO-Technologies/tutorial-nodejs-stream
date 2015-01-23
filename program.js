a581495b2a39276f89055564098471120d2bd963
JSO-Technologies <JSO.Technologies@gmail.com>
2015-01-23 14:45:25 +0100

    Combiner

--- program.js
+++ program.js
@@ -1,17 +1,39 @@
-var duplex = require('duplexer');
+var combine = require('stream-combiner');
+var split = require('split');
 var through = require('through');
+var zlib = require('zlib');
 
-module.exports = function(counter) {
-    var count = {};
+module.exports = function() {
+    var result = [];
+    var currentGenre;
+    var groupStream = through(
+        function(line) {
+            if (line.length === 0) return;
 
-    var inStream = through(
-        function(buff) {
-            count[buff.country] = (count[buff.country] || 0) + 1;
+            var obj = JSON.parse(line);
+            if(obj.type === 'genre') {
+                currentGenre = {
+                    name: obj.name,
+                    books: []
+                };
+                result.push(currentGenre);
+            }
+
+            else {
+                currentGenre.books.push(obj.name);
+            }
         },
         function() {
-            counter.setCounts(count);
+            this.queue(result.map(JSON.stringify).join('\n') + '\n');
+            this.queue(null);
         }
     );
 
-    return duplex(inStream, counter);
+    var print = through(
+        function(obj) {
+            console.log(obj);
+        }
+    );
+
+    return combine(split(), groupStream, zlib.createGzip());
 };
